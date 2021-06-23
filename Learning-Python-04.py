a40133fa6ba4a70cc0e455c0e89cd9eef5667b66
DanielQFK <DanialQueen2004@gmail.com>
2021-06-23 00:03:58 -0400

    Numbers...

---
+++ Learning-Python-04.py
@@ -0,0 +1,19 @@
+# Numbers
+# This part is about Numbers (Integers , Floats , Underscores in Numbers)
+# we can do many things with Integers
+# add =  + 
+# minus =  -  
+# multiply = * 
+# divide = /
+# exponen = **
+#  e.g. 
+
+# 1.
+print(2+2*3/4**2-10+1.5)
+
+#2.
+#You also use _ for high numbers.
+number = 1_500_000_000_000
+print(number)
+
+

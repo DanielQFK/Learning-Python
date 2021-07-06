d734387775dcccfdcc51ada777fc6557282536b2
DanielQFK <DanialQueen2004@gmail.com>
2021-07-06 03:15:30 -0400

    ...

---
+++ Learning-Python-09.py
@@ -0,0 +1,22 @@
+# input-int
+
+# what is input?
+# what is int?
+
+# e.g. 
+
+name = input()
+
+# "inpute" is used to ask for a variable...
+# output = what is your name ? >  Daniel (in this part you should enter your name)
+
+# Even you can explain what you want or not to make obvious that for someone who wants to 
+# follow what you want...
+name1 = input("What is your name? ") #Tip:anything between () is shown in output
+
+age = int(input())
+age1 = int(input("How old are you? "))
+
+# "int(input())" is used to ask for an integer...
+# output = How old are you ? > 17   (in this part you should enter your age)
+# Tip 1:Be careful int is only for number . if you enter variable it will have an error in output 

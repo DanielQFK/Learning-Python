db72f38db9c247130f3e007c05da41c7cccdd641
DanielQFK <DanialQueen2004@gmail.com>
2021-07-18 00:58:44 -0400

    Touple

---
+++ Learning-Python-11.py
@@ -0,0 +1,23 @@
+# touple
+
+# touples are so similar to lists but with a defference 
+
+# 1.You can change lists but you can't change touples
+# 2.You write list in [] but you write touple in ()
+
+Touple = ('alex' , 'max' , 'emma' , 'john')
+print(Touple)
+print(Touple[0])
+print(Touple[-1])
+
+Touple1 = (100)
+print(Touple1)
+
+Touple2 = (50 , 100 , 2)
+print(Touple2)
+
+Touple3 = (300 , 5)
+print(Touple3)
+
+# important tip:
+# You can not append/insert/sort/remove or... touples

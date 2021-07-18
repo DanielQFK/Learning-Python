9432800614964ce04362d10b613aa0b887e5d30f
Daniel <84660198+DanielQFK@users.noreply.github.com>
2021-07-18 01:10:20 -0400

    Tuple...

--- Learning-Python-11.py
+++ Learning-Python-11.py
@@ -1,23 +1,36 @@
-# touple
+# tuple
 
-# touples are so similar to lists but with a defference 
+# tuples are so similar to lists but with a defference 
 
 # 1.You can change lists but you can't change touples
-# 2.You write list in [] but you write touple in ()
+# 2.You write list in [] but you write tuple in ()
 
-Touple = ('alex' , 'max' , 'emma' , 'john')
-print(Touple)
-print(Touple[0])
-print(Touple[-1])
+Tuple = ('alex' , 'max' , 'emma' , 'john')
+print(Tuple)
+# output = ('alex', 'max', 'emma', 'john')
 
-Touple1 = (100)
-print(Touple1)
+print(Tuple[0])
+# output = alex
+print(Tuple[-1])
+# output = john
 
-Touple2 = (50 , 100 , 2)
-print(Touple2)
+Tuple1 = (100 , 324 , 534 , 54 , 123 , 342)
+print(Tuple1)
+# output = (100 , 324 , 534 , 54 , 123 , 342)
 
-Touple3 = (300 , 5)
-print(Touple3)
+Tuple2 = (50 , 100 , 2)
+print(Tuple2)
+# output = (50, 100, 2)
+
+Tuple3 = (300 , 5)
+print(Tuple3)
+# output = (300, 5)
 
 # important tip:
-# You can not append/insert/sort/remove or... touples
+# You can not append/insert/sort/remove or... tuples
+Tuple3.append(23)
+print(Tuple3)
+# # output = 
+# line 24, in <module>
+#     Tuple3.append(23)
+# AttributeError: 'tuple' object has no attribute 'append'

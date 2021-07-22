07d70cb8de67226bc648fa8ae17e42a22299da03
DanielQFK <DanialQueen2004@gmail.com>
2021-07-22 00:55:02 -0400

    while

---
+++ Learning-Python-13.py
@@ -0,0 +1,27 @@
+# while loop
+
+# While loops are so similar to for loops but in some differences
+# 1.
+# in while loop you should explain a condition which means it will stop 
+# when the condition isn't true . Until the condition is true it keeps on working ...
+# e.g.
+while "hello"=="hello":
+    a = input("your name ? ")
+# of course hello is the same hello so it's true (it keeps on to uncountable number)    
+# or
+while True==False:
+    a = input("Hi > ")
+# Now because it is not true nothing shows up in the output...
+#or
+x = 0
+while x!=5:
+    print("Hi")
+    x = x+1
+# But in this it will stop when x = 5    
+
+# 2.
+# But in For loop you don;t have to explain a condition , Just say a number to continue 
+# till it finishes... 
+
+# IMPORTANT TIP : Don't try while by printing something because it keeps on and it may 
+# damage your computer...    

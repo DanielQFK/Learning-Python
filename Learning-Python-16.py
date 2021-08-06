dce6d28d1f9eb8d948a004d95c3be3c148e04b1c
Daniel <84660198+DanielQFK@users.noreply.github.com>
2021-08-06 02:59:06 -0400

    def...

--- Learning-Python-16.py
+++ Learning-Python-16.py
@@ -57,3 +57,36 @@ print(Q)
 """Output:5"""  
 # Tips = in returning , return something... 
 # give a variable to your def and remember to write your numbers in () and print that variable  ...
+
+# Function part 2
+
+# Using a Function with a while Loop
+def Def1():
+    while True: # continues till uncountable amount
+        print("Hello , how are you? ")
+        answer = input(" > ")
+        print("\nPlease say your name:")
+        f_name = input("First name: ")
+        l_name = input("Last name: ")
+
+        Full_name = (f"{f_name}{l_name}")
+        print(Full_name)
+        break # It stops the while or if loop
+
+Def1()
+
+# Importing
+"""At first you should have the file you want to import in that folder
+then just write 
+from [name of the file] import [The name of def function in that file]
+from module_name import function_name
+from module_name import function_0, function_1, function_2
+"""
+# Using as to Give a Function an Alias
+"""from pizza import numpy as np
+np()
+np()"""
+
+#Importing All Functions in a Module
+"""from [[name of the file]] import *   * is important
+"""

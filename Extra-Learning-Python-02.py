aae526e87764d2afb7928c4c24068b128610d1f7
DanielQFK <DanialQueen2004@gmail.com>
2021-08-23 08:47:56 -0400

    ...

---
+++ Extra-Learning-Python-02.py
@@ -0,0 +1,16 @@
+# strip
+
+Name = " Alex "
+# as you see there is a space between "" and Alex :
+
+print(Name.strip())
+# strip.()  output = 'Alex' (The space deleted)
+
+print(Name.rstrip())
+# rstrip.() r=right , so output = ' Alex'
+
+print(Name.lstrip())
+# lstrip.() l=left , so output = 'Alex '
+# It is just used when you have written a string with space with "" 
+
+
